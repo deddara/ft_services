aa1581bb417e0c4f27d37cb416facf7346b043a0
relasel <deddara@student.21-school.ru>
2020-09-11 20:17:55 +0300

    k8s works

---
+++ setup.sh
@@ -0,0 +1,22 @@
+services=("nginx" "wordpress" "mysql")
+
+minikube start --vm-driver=virtualbox
+
+eval $(minikube docker-env) > /dev/null
+
+minikube addons enable metallb
+kubectl apply -f srcs/k8s_configs/metallb.yaml
+
+for service in "${services[@]}"
+do
+printf "docker build ${service}: "
+docker build srcs/${service} -t "${service}-image" > /dev/null
+printf "status - OK\n"
+done
+
+kubectl apply -f srcs/k8s_configs/volume.yaml
+
+for service in "${services[@]}"
+do
+kubectl apply -f srcs/k8s_configs/${service}.yaml
+done
